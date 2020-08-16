9e634d45a0782f19eadfe3c1ecb66fa1edbc62c1
efuller11 <ejfuller21@gmail.com>
2020-08-16 11:12:27 -0400

    add feature to comment on blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     # 127.0.0.1:8000/post2/publish
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 
-
+    # 127.0.0.1:800/post/2/comment
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 
 ]
